84ffefd0d2cdce3dc60dca5f21c988888bdcbe54
Tutku Ucan <mettheus0@gmail.com>
2024-10-26 02:24:31 +0300

    update channel page

---
+++ components/MemberCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<template>
+    <div class="member-card">
+        <div class="profile">
+            <Avatar label="T" shape="circle" />
+            <p>@{{ user.username }}</p>
+        </div>
+        <div class="role">
+            Member
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.member-card {
+    padding: 20px;
+    border-radius: 12px;
+    border: 0.5px solid gray;
+    display: flex;
+    align-items: center
+}
+</style>
--- pages/channel/[id].vue
+++ pages/channel/[id].vue
@@ -4,10 +4,10 @@ export default {
     components: {
         Channel
     },
-    data(){
+    data() {
         return {
             channels: [
-            {
+                {
                     id: 1,
                     name: "birinci kanal",
                     onlineCount: 12,
@@ -38,47 +38,52 @@ export default {
             channel: null
         }
     },
-    created(){
-        // this.channel: ChannelType = this.channels.find(chan => chan.id == this.$route.params.id)
-        console.log(this.$route.params.id)
+    created() {
+        this.channel = this.channels.find(chan => chan.id == this.$route.params.id)
     }
 }
 </script>
 <template>
     <div class="chat-template">
-        <section>
-            <Tabs value="0">
-                <TabList>
-                    <Tab value="0">My Channels</Tab>
-                    <Tab value="1">Joined Channels</Tab>
-                </TabList>
-                <TabPanels>
-                    <TabPanel value="0">
-                        <template v-for="channel in channels">
-                            <Channel :channel="channel" />
-                        </template>
-                    </TabPanel>
-                    <TabPanel value="1">
-                        <template v-for="channel in channels">
-                            <Channel :channel="channel" />
-                        </template>
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-        </section>
-        <main>
-            <div class="channel-header">
-                <Button icon="pi pi-arrow-left" severity="secondary" raised />
-                <p class="title">Channel 1</p>
-                <div></div>
-            </div>
-            <div class="chat-input">
-                <InputText type="text" v-model="text" variant="filled" />
-            </div>
-        </main>
-        <section>
-            <h2>Members</h2>
-        </section>
+        <Splitter>
+            <SplitterPanel :size="50" :minSize="10">
+                <Tabs value="0">
+                    <TabList>
+                        <Tab value="0">My Channels</Tab>
+                        <Tab value="1">Joined Channels</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel value="0">
+                            <template v-for="channel in channels">
+                                <Channel :channel="channel" />
+                            </template>
+                        </TabPanel>
+                        <TabPanel value="1">
+                            <template v-for="channel in channels">
+                                <Channel :channel="channel" />
+                            </template>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </SplitterPanel>
+            <SplitterPanel :size="100">
+                <div class="channel-header">
+                    <Button icon="pi pi-arrow-left" severity="secondary" raised />
+                    <p class="title">{{ channel.name }}</p>
+                    <div></div>
+                </div>
+                <div class="messages-area">
+
+                </div>
+                <div class="chat-input">
+                    <InputText type="text" placeholder="Text here.." v-model="text" variant="filled" />
+                    <Button icon="pi pi-send" severity="secondary" raised />
+                </div>
+            </SplitterPanel>
+            <SplitterPanel :size="50">
+                <h2>Members</h2>
+            </SplitterPanel>
+        </Splitter>
     </div>
 </template>
 
@@ -86,44 +91,80 @@ export default {
 .p-tablist-tab-list {
     background: none;
 }
-.p-tabpanels { 
+
+.p-tabpanels {
     background: none;
     padding: 20px 0;
 
 }
+
 .p-tabpanel {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
+    .router-link {
+        margin-bottom: 20px;
+    }
 }
+
 .chat-template {
     display: flex;
     flex-direction: column;
     height: 100vh;
+    width: 100%;
 
     main {
         flex: 1;
-    }
+        position: relative;
 
-    .channel-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
 
-        .title {
-            font-weight: bold;
-            font-size: 24px;
-        }
+
     }
 }
 
+.channel-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .title {
+        font-weight: bold;
+        font-size: 24px;
+    }
+}
+
+.messages-area {
+    height: 85%;
+}
+
+.chat-input {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .p-inputtext {
+        width: 100%;
+    }
+}
+
+.p-splitter {
+    height: 100vh;
+}
+.p-splitterpanel {
+    padding: 20px;
+}
 @media (min-width: 991px) {
     .chat-template {
-        flex-direction: row;
 
         section {
-            border: 1px solid lime;
             width: 30%;
+
+            &:first-child {
+                border-right: 0.5px solid rgb(34, 34, 34);
+            }
+
+            &:last-child {
+                border-left: 0.5px solid rgb(34, 34, 34);
+            }
         }
 
         main,
